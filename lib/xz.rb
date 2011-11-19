f683439628737b1b0f63a55c032a6ce3e6e28639
Christoph Plank <chrisistuff@gmail.com>
2011-11-19 16:16:23 +0100

    updated copyrights

--- lib/xz.rb
+++ lib/xz.rb
@@ -4,6 +4,7 @@
 Basic liblzma-bindings for Ruby.
 
 Copyright © 2011 Marvin Gülker
+Copyright © 2011 Christoph Plank
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the ‘Software’),
