761ac9e84b1be3a8bdca5aaca97a1b56f3c6207e
saki7 <n.sakisaka@gmail.com>
2014-02-08 22:41:05 +0900

    Remove trailing spaces

--- test/test_xz.rb
+++ test/test_xz.rb
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 # (The MIT License)
-# 
+#
 # Basic unit-tests for the liblzma-bindings for Ruby.
-# 
+#
 # Copyright © 2011,2012 Marvin Gülker
 # Copyright © 2011 Christoph Plank
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the ‘Software’),
 # to deal in the Software without restriction, including without limitation
 # the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom the Software
 # is furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED ‘AS IS’, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,12 +27,12 @@
 require_relative "./common"
 
 class TestXZ < Minitest::Test
-  
+
   TEST_XZ = "\3757zXZ\000\000\004\346\326\264F\002\000!\001\026\000\000\000t/" +
   "\345\243\340\000\023\000\020]\000\030\fB\222jg\274\016\32132a\326|\000\000" +
   "\000\017:\376\373\"1\270\266\000\001,\024\370\nm\003\037\266\363}\001\000" +
   "\000\000\000\004YZ"
-  
+
   def test_decompress
     assert_equal(XZ.decompress(TEST_XZ), '01234567890123456789')
   end
