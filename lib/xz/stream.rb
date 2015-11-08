e39f67f21b8a8dbe4eb21fe4cafdb4d74ceeac86
Quintus <quintus@quintilianus.eu>
2015-11-08 20:40:01 +0100

    Fix formatting of the documentation comments.
    Sins from the early days.

--- lib/xz/stream.rb
+++ lib/xz/stream.rb
@@ -23,29 +23,29 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-#The base class for XZ::StreamReader and XZ::StreamWriter.
-#This is an abstract class that is not meant to be used
-#directly; if you try, you will soon recognise that you’ve
-#created a quite limited object ;-). You can, however, test
-#against this class in <tt>kind_of?</tt> tests.
+# The base class for XZ::StreamReader and XZ::StreamWriter.
+# This is an abstract class that is not meant to be used
+# directly; if you try, you will soon recognise that you’ve
+# created a quite limited object ;-). You can, however, test
+# against this class in <tt>kind_of?</tt> tests.
 #
-#XZ::StreamReader and XZ::StreamWriter are IO-like classes that
-#allow you to access XZ-compressed data the same way you access
-#an IO-object, easily allowing to fool other libraries that expect
-#IO objects. The most noticable example for this may be reading
-#and writing XZ-compressed tarballs; see XZ::StreamReader and
-#XZ::StreamWriter for respective examples.
+# XZ::StreamReader and XZ::StreamWriter are IO-like classes that
+# allow you to access XZ-compressed data the same way you access
+# an IO-object, easily allowing to fool other libraries that expect
+# IO objects. The most noticable example for this may be reading
+# and writing XZ-compressed tarballs; see XZ::StreamReader and
+# XZ::StreamWriter for respective examples.
 #
-#Neither this class nor its subclasses document the IO-methods
-#they contain--this is due to the reason that they include the
-#great IO::Like module that provides all the necessary IO methods
-#based on a few methods you define. For all defined IO methods,
-#see the +io-like+ gem’s documentation.
+# Neither this class nor its subclasses document the IO-methods
+# they contain--this is due to the reason that they include the
+# great IO::Like module that provides all the necessary IO methods
+# based on a few methods you define. For all defined IO methods,
+# see the +io-like+ gem’s documentation.
 class XZ::Stream
   include IO::Like
 
-  #Creates a new instance of this class. Don’t use this directly,
-  #it’s only called by subclasses’ ::new methods.
+  # Creates a new instance of this class. Don’t use this directly,
+  # it’s only called by subclasses’ ::new methods.
   def initialize(delegate_io)
     @delegate_io    = delegate_io
     @lzma_stream    = XZ::LZMAStream.new
@@ -53,10 +53,10 @@ class XZ::Stream
 
   private
 
-  #This method returns the size of +str+ in bytes.
+  # This method returns the size of +str+ in bytes.
   def binary_size(str)
-    #Believe it or not, but this is faster than str.bytes.to_a.size.
-    #I benchmarked it, and it is as twice as fast.
+    # Believe it or not, but this is faster than str.bytes.to_a.size.
+    # I benchmarked it, and it is as twice as fast.
     if str.respond_to? :force_encoding
       str.dup.force_encoding(Encoding::BINARY).size
     else
--- lib/xz/stream_reader.rb
+++ lib/xz/stream_reader.rb
@@ -23,81 +23,92 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-#An IO-like reader class for XZ-compressed data, allowing you to
-#access XZ-compressed data as if it was a normal IO object, but
-#please note you can’t seek in the data--this doesn’t make much
-#sense anyway. Where would you want to seek? The plain or the XZ
-#data?
+# An IO-like reader class for XZ-compressed data, allowing you to
+# access XZ-compressed data as if it was a normal IO object, but
+# please note you can’t seek in the data--this doesn’t make much
+# sense anyway. Where would you want to seek? The plain or the XZ
+# data?
 #
-#A StreamReader object actually wraps another IO object it reads
-#the compressed data from; you can either pass this IO object directly
-#to the ::new method, effectively allowing you to pass any IO-like thing
-#you can imagine (just ensure it is readable), or you can pass a path
-#to a filename to ::new, in which case StreamReader takes care of both
-#opening and closing the file correctly. You can even take it one step
-#further and use the block form of ::new which will automatically call
-#the #close method for you after the block finished. However, if you pass
-#an IO, remember you have to close:
+# A StreamReader object actually wraps another IO object it reads
+# the compressed data from; you can either pass this IO object directly
+# to the ::new method, effectively allowing you to pass any IO-like thing
+# you can imagine (just ensure it is readable), or you can pass a path
+# to a filename to ::new, in which case StreamReader takes care of both
+# opening and closing the file correctly. You can even take it one step
+# further and use the block form of ::new which will automatically call
+# the #close method for you after the block finished. However, if you pass
+# an IO, remember you have to close:
 #
-#1. The StreamReader instance.
-#2. The IO object you passed to ::new.
+# 1. The StreamReader instance.
+# 2. The IO object you passed to ::new.
 #
-#Do it <b>in exactly that order</b>, otherwise you may lose data.
+# Do it <b>in exactly that order</b>, otherwise you may lose data.
 #
-#See the +io-like+ gem’s documentation for the IO-reading methods
-#available for this class (although you’re probably familiar with
-#them through Ruby’s own IO class ;-)).
+# See the +io-like+ gem’s documentation for the IO-reading methods
+# available for this class (although you’re probably familiar with
+# them through Ruby’s own IO class ;-)).
 #
-#==Example
-#In this example, we’re going to use ruby-xz together with the
-#+archive-tar-minitar+ gem that allows to read tarballs. Used
-#together, the two libraries allow us to read XZ-compressed tarballs.
+# ==Example
+# In this example, we’re going to use ruby-xz together with the
+# +archive-tar-minitar+ gem that allows to read tarballs. Used
+# together, the two libraries allow us to read XZ-compressed tarballs.
 #
-#  require "xz"
-#  require "archive/tar/minitar"
+#   require "xz"
+#   require "archive/tar/minitar"
 #
-#  XZ::StreamReader.open("foo.tar.xz") do |txz|
-#    # This automatically closes txz
-#    Archive::Tar::Minitar.unpack(txz, "foo")
-#  end
+#   XZ::StreamReader.open("foo.tar.xz") do |txz|
+#     # This automatically closes txz
+#     Archive::Tar::Minitar.unpack(txz, "foo")
+#   end
 class XZ::StreamReader < XZ::Stream
 
-  #The memory limit you set for this reader (in ::new).
+  # The memory limit you set for this reader (in ::new).
   attr_reader :memory_limit
-  #The flags you set for this reader (in ::new).
+
+  # The flags you set for this reader (in ::new).
   attr_reader :flags
 
-  #call-seq:
-  #  new(delegate, memory_limit = XZ::LibLZMA::UINT64_MAX, flags = [:tell_unsupported_check])  → a_stream_reader
-  #  open(delegate, memory_limit = XZ::LibLZMA::UINT64_MAX, flags = [:tell_unsupported_check]) → a_stream_reader
+  # call-seq:
+  #   new(delegate, memory_limit = XZ::LibLZMA::UINT64_MAX, flags = [:tell_unsupported_check])  → a_stream_reader
+  #   open(delegate, memory_limit = XZ::LibLZMA::UINT64_MAX, flags = [:tell_unsupported_check]) → a_stream_reader
   #
-  #Creates a new StreamReader instance. If you pass an IO,
-  #remember you have to close *both* the resulting instance
-  #(via the #close method) and the IO object you pass to flush
-  #any internal buffers in order to be able to read all decompressed
-  #data.
-  #==Parameters
-  #[delegate] An IO object to read the data from, or a path
-  #           to a file to open. If you’re in an urgent need to
-  #           pass a plain string, use StringIO from Ruby’s
-  #           standard library. If this is an IO, it must be
-  #           opened for reading.
-  #The other parameters are identical to what the XZ::decompress_stream
-  #method expects.
-  #==Return value
-  #The newly created instance.
-  #==Example
-  #  # Wrap it around a file
-  #  f = File.open("foo.xz")
-  #  r = XZ::StreamReader.new(f)
+  # Creates a new StreamReader instance. If you pass an IO,
+  # remember you have to close *both* the resulting instance
+  # (via the #close method) and the IO object you pass to flush
+  # any internal buffers in order to be able to read all decompressed
+  # data.
   #
-  #  # Ignore any XZ checksums (may result in invalid data being read!)
-  #  File.open("foo.xz") do |f|
-  #    r = XZ::StreamReader.new(f, XZ::LibLZMA::UINT64_MAX, [:tell_no_check]
-  #  end
+  # === Parameters
   #
-  #  # Let StreamReader handle file closing automatically
-  #  XZ::StreamReader.new("myfile.xz"){|r| r.raed}
+  # [delegate]
+  #   An IO object to read the data from, or a path
+  #   to a file to open. If you’re in an urgent need to
+  #   pass a plain string, use StringIO from Ruby’s
+  #   standard library. If this is an IO, it must be
+  #   opened for reading.
+  #
+  # The other parameters are identical to what the XZ::decompress_stream
+  # method expects.
+  #
+  # === Return value
+  #
+  # The newly created instance.
+  #
+  # === Example
+  #
+  #   # Wrap it around a file
+  #   f = File.open("foo.xz")
+  #   r = XZ::StreamReader.new(f)
+  #
+  #   # Ignore any XZ checksums (may result in invalid
+  #   # data being read!)
+  #   File.open("foo.xz") do |f|
+  #     r = XZ::StreamReader.new(f, XZ::LibLZMA::UINT64_MAX, [:tell_no_check]
+  #   end
+  #
+  #   # Let StreamReader handle file closing
+  #   # automatically
+  #   XZ::StreamReader.new("myfile.xz"){|r| r.raed}
   def initialize(delegate, memory_limit = XZ::LibLZMA::UINT64_MAX, flags = [:tell_unsupported_check])
     raise(ArgumentError, "Invalid memory limit set!") unless (0..XZ::LibLZMA::UINT64_MAX).include?(memory_limit)
     flags.each do |flag|
@@ -135,15 +146,21 @@ class XZ::StreamReader < XZ::Stream
   end
   self.class.send(:alias_method, :open, :new)
 
-  #Closes this StreamReader instance. Don’t use it afterwards
-  #anymore.
-  #==Return value
-  #The total number of bytes decompressed.
-  #==Example
-  #  r.close #=> 6468
-  #==Remarks
-  #If you passed an IO to ::new, this method doesn’t close it, so
-  #you have to close it yourself.
+  # Closes this StreamReader instance. Don’t use it afterwards
+  # anymore.
+  #
+  # === Return value
+  #
+  # The total number of bytes decompressed.
+  #
+  # === Example
+  #
+  #   r.close #=> 6468
+  #
+  # === Remarks
+  #
+  # If you passed an IO to ::new, this method doesn’t close it, so
+  # you have to close it yourself.
   def close
     super
 
@@ -158,36 +175,41 @@ class XZ::StreamReader < XZ::Stream
     @lzma_stream[:total_out]
   end
 
-  #call-seq:
-  #  pos()  → an_integer
-  #  tell() → an_integer
+  # call-seq:
+  #   pos()  → an_integer
+  #   tell() → an_integer
   #
-  #Total number of output bytes provided to you yet.
+  # Total number of output bytes provided to you yet.
   def pos
     @lzma_stream[:total_out]
   end
   alias tell pos
 
-  #Instrcuts liblzma to immediately stop decompression,
-  #rewinds the wrapped IO object and reinitalizes the
-  #StreamReader instance with the same values passed
-  #originally to the ::new method. The wrapped IO object
-  #must support the +rewind+ method for this method to
-  #work; if it doesn’t, this method throws an IOError.
-  #After the exception was thrown, the StreamReader instance
-  #is in an unusable state. You cannot continue using it
-  #(don’t call #close on it either); close the wrapped IO
-  #stream and create another instance of this class.
-  #==Raises
-  #[IOError] The wrapped IO doesn’t support rewinding.
-  #          Do not use the StreamReader instance anymore
-  #          after receiving this exception.
-  #==Remarks
-  #I don’t really like this method, it uses several dirty
-  #tricks to circumvent both io-like’s and liblzma’s control
-  #mechanisms. I only implemented this because the
-  #<tt>archive-tar-minitar</tt> gem calls this method when
-  #unpacking a TAR archive from a stream.
+  # Instrcuts liblzma to immediately stop decompression,
+  # rewinds the wrapped IO object and reinitalizes the
+  # StreamReader instance with the same values passed
+  # originally to the ::new method. The wrapped IO object
+  # must support the +rewind+ method for this method to
+  # work; if it doesn’t, this method throws an IOError.
+  # After the exception was thrown, the StreamReader instance
+  # is in an unusable state. You cannot continue using it
+  # (don’t call #close on it either); close the wrapped IO
+  # stream and create another instance of this class.
+  #
+  # === Raises
+  #
+  # [IOError]
+  #   The wrapped IO doesn’t support rewinding.
+  #   Do not use the StreamReader instance anymore
+  #   after receiving this exception.
+  #
+  # ==Remarks
+  #
+  # I don’t really like this method, it uses several dirty
+  # tricks to circumvent both io-like’s and liblzma’s control
+  # mechanisms. I only implemented this because the
+  # <tt>archive-tar-minitar</tt> gem calls this method when
+  # unpacking a TAR archive from a stream.
   def rewind
     # HACK: Wipe all data from io-like’s internal read buffer.
     # This heavily relies on io-like’s internal structure.
@@ -212,18 +234,18 @@ class XZ::StreamReader < XZ::Stream
     initialize(@delegate_io, @memory_limit, @flags)
   end
 
-  #NO, you CANNOT seek in this object!!
-  #io-like’s default behaviour is to raise Errno::ESPIPE
-  #when calling a non-defined seek, which is not what some
-  #libraries such as RubyGem’s TarReader expect (they expect
-  #a NoMethodError/NameError instead).
+  # NO, you CANNOT seek in this object!!
+  # io-like’s default behaviour is to raise Errno::ESPIPE
+  # when calling a non-defined seek, which is not what some
+  # libraries such as RubyGem’s TarReader expect (they expect
+  # a NoMethodError/NameError instead).
   undef seek
 
   private
 
-  #Called by io-like’s read methods such as #read. Does the heavy work
-  #of feeding liblzma the compressed data and reading the returned
-  #uncompressed data.
+  # Called by io-like’s read methods such as #read. Does the heavy work
+  # of feeding liblzma the compressed data and reading the returned
+  # uncompressed data.
   def unbuffered_read(length)
     raise(EOFError, "Input data completely processed!") if @__lzma_finished
 
